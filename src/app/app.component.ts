66f281dce2a9a780efc69a5cf31b3038557aca56
Rohankumar <rohankyatham02@gmail.com>
2021-05-09 15:14:53 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // status:boolean=true;
+  // changeStatus(){
+  //   this.status=!this.status;
+  // }
+
+
+  // listOfUser:string[]=[];
+  // addItem(item){
+  //   //push into array
+  //   this.listOfUser.push(item)
+
+  //}
+  // Users=[];
+
+  // addUserDetails(ref){
+    
+
+  //   let userObj=ref.value;
+    
+  //   //push into users array
+  //   this.Users.push(userObj);
+
+  //   //clear form flields
+  //   ref.reset();
+  // };
+  // deleteItem(ind){
+  //   this.Users.splice(ind,1);
+  // };
+}
